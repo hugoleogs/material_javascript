3e156396c70714406a5e41ad136713c3256c1437
Hugo <hugoleonardo.gs2@gmail.com>
2020-06-25 08:32:33 +0000

    Exercicio js edabit

---
+++ exercicios/factor_chain.js
@@ -0,0 +1,46 @@
+/*
+Factor Chain
+
+A factor chain is an array where each previous element is a factor of the next consecutive element. The following is a factor chain:
+
+[3, 6, 12, 36]
+
+// 3 is a factor of 6
+// 6 is a factor of 12
+// 12 is a factor of 36
+
+Create a function that determines whether or not an array is a factor chain.
+Examples
+
+factorChain([1, 2, 4, 8, 16, 32]) ➞ true
+
+factorChain([1, 1, 1, 1, 1, 1]) ➞ true
+
+factorChain([2, 4, 6, 7, 12]) ➞ false
+
+factorChain([10, 1]) ➞ false
+
+Notes
+
+N/A
+
+*/
+
+function factorChain(arr) {
+    let saida = true
+    for (let i = 1; i < arr.length; i++){
+        if ((arr[i]%arr[i-1])){
+            saida = false
+            break
+        }
+    }	
+    return saida
+}
+
+console.log(factorChain([1, 2, 4, 8, 16, 32]))
+
+console.log(factorChain([1, 1, 1, 1, 1, 1]))
+
+console.log(factorChain([2, 4, 6, 7, 12]))
+
+console.log(factorChain([10, 1]))

34c8746e6fe34eee6350ec0abb41df1383d8d593
Hugo <hugoleonardo.gs2@gmail.com>
2020-07-08 10:30:01 +0000

    Exercicios js do edabit

---
+++ exercicios/array_of_Multiples.js
@@ -0,0 +1,33 @@
+/*
+Array of Multiples
+
+Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num up to length.
+Examples
+
+arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+
+arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+
+arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+
+Notes
+
+Notice that num is also included in the returned array.
+
+URL: https://edabit.com/challenge/ebcd4Xu8TLizaj6dm
+
+*/
+
+function arrayOfMultiples (num, length) {
+    let saida = []
+    for(let i = 1; i < length+1; i++){
+        saida.push(num*i)
+    }
+    return saida
+}
+
+console.log(arrayOfMultiples(7, 5))
+
+console.log(arrayOfMultiples(12, 10))
+
+console.log(arrayOfMultiples(17, 6))

087eb622ae9a56000c38d35ca70cfdc584d6c2aa
Hugo <hugoleonardo.gs2@gmail.com>
2020-07-15 23:49:27 +0000

    exercicio de js do edabit

--- exercicios/pricey_Products.js
+++ exercicios/pricey_Products.js
@@ -10,6 +10,8 @@ products({"Bike1" : 510, "Bike2" : 401, "Bike3" : 501}) ➞ ["Bike1", "Bike3"])
 
 products({"Loafers" : 50, "Vans" : 10, "Crocs" : 20}) ➞ []
 
+products({"Calvin Klein" : 500, "Armani" : 5000, "Dolce & Gabbana" : 2000}), ["Armani", "Dolce & Gabbana", "Calvin Klein"])
+
 Notes
 
 N/A
@@ -19,7 +21,7 @@ URL: https://edabit.com/challenge/neWTApTYread9ZNdP
 */
 
 function products(obj) {
-	return (((Object.entries(obj)).filter(i => i[1]>500)).sort((e, b) => e[1]>b[1]? -1 : 1)).map(a => a[0])
+	return (((Object.entries(obj)).filter(i => i[1]>=500)).sort((e, b) => e[1]>b[1]? -1 : 1)).map(a => a[0])
 }
 
 console.log(products({"Computer" : 600, "TV" : 800, "Radio" : 50}))
@@ -27,3 +29,5 @@ console.log(products({"Computer" : 600, "TV" : 800, "Radio" : 50}))
 console.log(products({"Bike1" : 510, "Bike2" : 401, "Bike3" : 501}))
 
 console.log(products({"Loafers" : 50, "Vans" : 10, "Crocs" : 20}))
+
+console.log(products({"Calvin Klein" : 500, "Armani" : 5000, "Dolce & Gabbana" : 2000}))

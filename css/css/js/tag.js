0d1131500fc42cf07008001d56c05bd5c5c73139
Hugo <hugoleonardo.gs2@gmail.com>
2021-01-18 16:06:21 -0300

    estudo Udemy

---
+++ css/css/js/tag.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.tag').forEach(elemento => {
+    const tagName = elemento.tagName.toLowerCase()
+
+    elemento.getElementsByClassName.borderColor = '#616161'
+
+    if(!elemento.classList.contains('nolabel')){
+
+        const label = document.createElement('label')
+        label.style.backgroundColor = '#616161'
+        label.innerHTML = tagName
+        elemento.insertBefore(label, elemento.childNodes[0])
+
+
+    }
+})

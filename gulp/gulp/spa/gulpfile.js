95c44657f04409a52cc01504d86c4f522487257f
Hugo <hugoleonardo.gs2@gmail.com>
2021-03-15 07:36:08 -0300

    atividade Udemy

---
+++ gulp/gulp/spa/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const util = require('gulp-util')
+const {series, parallel} = require('gulp')
+
+const{appHTML, appCSS, appJS, appIMG} = require('./gulpTasks/app')
+const{depsCSS, depsFonts} = require('./gulpTasks/deps')
+const{servidor, monitorarArquivos} = require('./gulpTasks/servidor')
+
+module.exports.default = series(
+
+    parallel(
+        series(appHTML, appCSS, appJS, appIMG),
+        series(depsCSS, depsFonts)
+    ),
+    servidor,
+    monitorarArquivos
+)
